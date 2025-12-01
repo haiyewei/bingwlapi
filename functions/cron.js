cab7a6e749e1b2d24f2e174acd422be98f389dff
haiyewei <79199415+haiyewei@users.noreply.github.com>
2025-12-01 23:48:33 +0800

    update

--- functions/cron.js
+++ functions/cron.js
@@ -34,62 +34,60 @@ async function triggerWorkflowDispatch(owner, repo, workflow_id, token) {
   }
 }
 
-export default {
-  async scheduled(event, env, ctx) {
-    const GITHUB_TOKEN = env.GITHUB_TOKEN;
-    const GITHUB_OWNER = env.GITHUB_USER;
-    const GITHUB_REPO = env.GITHUB_REPO;
+export const scheduled = async (controller, env, ctx) => {
+  const GITHUB_TOKEN = env.GITHUB_TOKEN;
+  const GITHUB_OWNER = env.GITHUB_USER;
+  const GITHUB_REPO = env.GITHUB_REPO;
 
-    if (!GITHUB_TOKEN || !GITHUB_REPO || !GITHUB_OWNER) {
-      console.error(
-        "Missing required secrets: GITHUB_TOKEN, GITHUB_REPO, GITHUB_USER (as owner)",
-      );
-      return;
-    }
+  if (!GITHUB_TOKEN || !GITHUB_REPO || !GITHUB_OWNER) {
+    console.error(
+      "Missing required secrets: GITHUB_TOKEN, GITHUB_REPO, GITHUB_USER (as owner)",
+    );
+    return;
+  }
 
-    let workflow_id = null;
+  let workflow_id = null;
 
-    switch (event.cron) {
-      // UTC time: 00:05, Beijing time: 08:05
-      case "5 0 * * *":
-        workflow_id = "update-en-GB.yml";
-        break;
-      // UTC time: 05:05, Beijing time: 13:05
-      case "5 5 * * *":
-        workflow_id = "update-en-US-en-CA.yml";
-        break;
-      // UTC time: 15:05, Beijing time: 23:05
-      case "5 15 * * *":
-        workflow_id = "update-ja-JP.yml";
-        break;
-      // UTC time: 16:05, Beijing time: 00:05 (+1 day)
-      case "5 16 * * *":
-        workflow_id = "update-zh-CN.yml";
-        break;
-      // UTC time: 23:05, Beijing time: 07:05 (+1 day)
-      case "5 23 * * *":
-        workflow_id = "update-de-DE-fr-FR-it-IT.yml";
-        break;
-      // UTC time: 18:35, Beijing time: 02:35 (+1 day)
-      case "35 18 * * *":
-        workflow_id = "update-en-IN.yml";
-        break;
-      default:
-        console.log("No matching cron schedule");
-        break;
-    }
+  switch (controller.cron) {
+    // UTC time: 00:05, Beijing time: 08:05
+    case "5 0 * * *":
+      workflow_id = "update-en-GB.yml";
+      break;
+    // UTC time: 05:05, Beijing time: 13:05
+    case "5 5 * * *":
+      workflow_id = "update-en-US-en-CA.yml";
+      break;
+    // UTC time: 15:05, Beijing time: 23:05
+    case "5 15 * * *":
+      workflow_id = "update-ja-JP.yml";
+      break;
+    // UTC time: 16:05, Beijing time: 00:05 (+1 day)
+    case "5 16 * * *":
+      workflow_id = "update-zh-CN.yml";
+      break;
+    // UTC time: 23:05, Beijing time: 07:05 (+1 day)
+    case "5 23 * * *":
+      workflow_id = "update-de-DE-fr-FR-it-IT.yml";
+      break;
+    // UTC time: 18:35, Beijing time: 02:35 (+1 day)
+    case "35 18 * * *":
+      workflow_id = "update-en-IN.yml";
+      break;
+    default:
+      console.log("No matching cron schedule");
+      break;
+  }
 
-    if (workflow_id) {
-      console.log(`Triggering workflow: ${workflow_id}`);
-      ctx.waitUntil(
-        triggerWorkflowDispatch(
-          GITHUB_OWNER,
-          GITHUB_REPO,
-          workflow_id,
-          GITHUB_TOKEN,
-        ),
-      );
-    }
-    console.log("cron processed");
-  },
+  if (workflow_id) {
+    console.log(`Triggering workflow: ${workflow_id}`);
+    ctx.waitUntil(
+      triggerWorkflowDispatch(
+        GITHUB_OWNER,
+        GITHUB_REPO,
+        workflow_id,
+        GITHUB_TOKEN,
+      ),
+    );
+  }
+  console.log("cron processed");
 };
